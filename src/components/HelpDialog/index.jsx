929d69eb3a1966aafe7f9ac50e2284f4db350e08
D Otasek <dotasek.dev@gmail.com>
2020-12-01 14:18:23 -0500

    Add images to Help Dialog

--- src/components/HelpDialog/index.jsx
+++ src/components/HelpDialog/index.jsx
@@ -12,11 +12,20 @@ import Button from '@material-ui/core/Button'
 
 import { Typography } from '@material-ui/core';
 
+import HelpRLIPP from '../../assets/images/help_rlipp.png';
+import HelpVNN from '../../assets/images/help_vnn.png';
+
 const useStyles = makeStyles({
   closeButton: {
     width: "fit-content",
     alignSelf: "flex-end",
     margin: '0.5em'
+  },
+  helpImage: {
+    width: '90%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
   }
 });
 
@@ -41,6 +50,9 @@ const HelpDialog = (props) => {
           Visible Neural Networks (VNN)
         </Typography>
         <p>
+        <img src={HelpVNN} className={classes.helpImage}></img>
+        </p>
+        <p>
           <Typography>
         Visible Neural Network (VNN) is an interpretable deep learning framework designed to solve biomedical problems. DrugCell models have two branches, one learns from the chemical structure of drugs and the other learns from genotypes of cell lines defined by mutations to infer cell response to drugs. The interpretability comes from the biologically meaningful structure of the genotype branch, which is the Gene Ontology (GO), a hierarchical organization of cell functions, in the current version the DrugCell model. Each pathway (subsystem) in the GO is assigned with a set of a fixed number of neurons and the connectivity between neurons are determined by the hierarchical relationship between the pathways that the neurons are representing. 
         </Typography></p>
@@ -48,6 +60,9 @@ const HelpDialog = (props) => {
         <Typography variant="h6">
         Relative Local Improvement in Predictive Power (RLIPP)
         </Typography>
+        <p>
+        <img src={HelpRLIPP} className={classes.helpImage}></img>
+        </p>
         <p>
         <Typography>
         Relative Local Improvement in Predictive Power (RLIPP) scores quantify the importance of each subsystem in the Gene Ontology embedded in the DrugCell model. To calculate the RLIPP score for each subsystem, we trained and compared two different L2-norm penalized linear regression models of drug response local to that subsystem. The first model is trained using the neuron values representing the state of the subsystem given different genotypes. The second model is trained using the concatenation of the neuron values of the subsystem’s children. The RLIPP score is defined as the ratio of the predictive power of the first linear model to that of the second linear model indicating the importance of the parent subsystem in learning. RLIPP &gt; 1 reflects that the neuron values of the parent subsystem are more predictive for drug drug response than the mere concatenation of the neuron values of its children. 
