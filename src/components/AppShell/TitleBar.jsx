85e875a75204a89c0da33de254feb0e6dfa52181
D Otasek <dotasek.dev@gmail.com>
2020-12-08 17:42:20 -0500

    Update subtitle

--- src/components/AppShell/TitleBar.jsx
+++ src/components/AppShell/TitleBar.jsx
@@ -107,7 +107,7 @@ class TitleBar extends React.Component {
                     DrugCell Oracle: VNN Browser
                   </Typography> 
                   <Typography variant="subtitle1" color="inherit" noWrap={true} className={classes.descriptionText} >
-                    Discover synergistic target pathways of 640 drugs identified by DrugCell
+                  Browse response pathways and synergistic targets for database of 640 drugs
                   </Typography>
                   </Button>
               </div>
